5429a4c745047d4178641ad42b30106571c56e0d
bin <wubin@jooz.vip>
2023-06-18 15:17:17 +0800

    ...

---
+++ flutter_web_optimizer.c8af5066.js
@@ -0,0 +1,167 @@
+    // The code below is injected by flutter web optimizer, do not edit!!!!!!
+    
+    // window.assetBase
+    var assetBase = "https://ibinbro.github.io/bnsplit/";
+    
+    // window.mainjsManifest
+    var mainjsManifest = {
+  "main.dart.a3ccb398.b905917d.js": "main.dart.a3ccb398.b905917d.b905917d.js",
+  "main.dart.f11e1cc9.js": "main.dart.f11e1cc9.4e3284bf.js",
+  "main.dart.js": "main.dart.7eb29a33.js",
+  "main.dart_0.094ece36.js": "main.dart_0.094ece36.094ece36.js",
+  "main.dart_0.1b59f952.1b59f952.js": "main.dart_0.1b59f952.1b59f952.1b59f952.js",
+  "main.dart_0.js": "main.dart_0.787113bd.js",
+  "main.dart_1.251a3f7a.js": "main.dart_1.251a3f7a.251a3f7a.js",
+  "main.dart_1.f75715fe.f75715fe.js": "main.dart_1.f75715fe.f75715fe.f75715fe.js",
+  "main.dart_1.js": "main.dart_1.5c14e2d1.js",
+  "main.dart_2.1b1e90ab.1b1e90ab.js": "main.dart_2.1b1e90ab.1b1e90ab.1b1e90ab.js",
+  "main.dart_2.bae9f07e.js": "main.dart_2.bae9f07e.bae9f07e.js",
+  "main.dart_2.js": "main.dart_2.bf153669.js",
+  "main.dart_3.868224f7.868224f7.js": "main.dart_3.868224f7.868224f7.868224f7.js",
+  "main.dart_3.9c552fa3.js": "main.dart_3.9c552fa3.9c552fa3.js",
+  "main.dart_3.js": "main.dart_3.6c5dd5b4.js",
+  "main.dart_4.8d0aa7d0.8d0aa7d0.js": "main.dart_4.8d0aa7d0.8d0aa7d0.8d0aa7d0.js",
+  "main.dart_4.90e15d26.js": "main.dart_4.90e15d26.90e15d26.js",
+  "main.dart_4.js": "main.dart_4.8556c2be.js",
+  "main.dart_5.72d494bc.8084f0a9.js": "main.dart_5.72d494bc.8084f0a9.8084f0a9.js",
+  "main.dart_5.d1e12793.js": "main.dart_5.d1e12793.a4903ed9.js",
+  "main.dart_5.js": "main.dart_5.c6be6070.js"
+};
+    
+    // window.hashFileManifest
+    var hashFileManifest = {
+  "assets/AssetManifest.json": "assets/AssetManifest.a6e50d20.json",
+  "assets/FontManifest.json": "assets/FontManifest.d7517139.json",
+  "assets/assets/b1.webp": "assets/assets/b1.2497759d.webp",
+  "assets/assets/b2.webp": "assets/assets/b2.6782314e.webp",
+  "assets/assets/b3.webp": "assets/assets/b3.5524547f.webp",
+  "assets/assets/b4.webp": "assets/assets/b4.17d8a929.webp",
+  "assets/assets/b5.webp": "assets/assets/b5.d2b8f8b1.webp",
+  "assets/assets/b6.webp": "assets/assets/b6.a10c348c.webp",
+  "assets/assets/buy_vip_back.webp": "assets/assets/buy_vip_back.30d5e3c5.webp",
+  "assets/assets/buy_vip_card_top.webp": "assets/assets/buy_vip_card_top.5bd8ba5f.webp",
+  "assets/assets/cube_left.webp": "assets/assets/cube_left.9ee8e8e1.webp",
+  "assets/assets/cube_left_2.webp": "assets/assets/cube_left_2.455f653b.webp",
+  "assets/assets/cube_right.webp": "assets/assets/cube_right.b1356f65.webp",
+  "assets/assets/cube_right_2.webp": "assets/assets/cube_right_2.4976700b.webp",
+  "assets/assets/s_share.webp": "assets/assets/s_share.38293214.webp",
+  "assets/assets/ss_share.webp": "assets/assets/ss_share.e0888522.webp",
+  "assets/assets/sssvip_back.webp": "assets/assets/sssvip_back.f350d03a.webp",
+  "assets/assets/sssvip_card.webp": "assets/assets/sssvip_card.5b7bdef6.webp",
+  "assets/assets/sssvip_card_back.webp": "assets/assets/sssvip_card_back.81502425.webp",
+  "assets/assets/sssvip_id.webp": "assets/assets/sssvip_id.59b833c5.webp",
+  "assets/assets/sssvip_top.webp": "assets/assets/sssvip_top.d0f22c80.webp",
+  "assets/assets/ssvip_back.webp": "assets/assets/ssvip_back.b1a739ef.webp",
+  "assets/assets/ssvip_card.webp": "assets/assets/ssvip_card.4a6f15fb.webp",
+  "assets/assets/ssvip_card_back.webp": "assets/assets/ssvip_card_back.9658f4e5.webp",
+  "assets/assets/ssvip_id.webp": "assets/assets/ssvip_id.2a02ef6c.webp",
+  "assets/assets/ssvip_top.webp": "assets/assets/ssvip_top.e8d3dec6.webp",
+  "assets/assets/svip_back.webp": "assets/assets/svip_back.a4582aa0.webp",
+  "assets/assets/svip_card.webp": "assets/assets/svip_card.eb484843.webp",
+  "assets/assets/svip_card_back.webp": "assets/assets/svip_card_back.3bb6417f.webp",
+  "assets/assets/svip_id.webp": "assets/assets/svip_id.0a0b71d4.webp",
+  "assets/assets/svip_top.webp": "assets/assets/svip_top.ab6a89bc.webp",
+  "assets/assets/vip/1.webp": "assets/assets/vip/1.01ede943.webp",
+  "assets/assets/vip/10.webp": "assets/assets/vip/10.be8b0089.webp",
+  "assets/assets/vip/11.webp": "assets/assets/vip/11.c31d7547.webp",
+  "assets/assets/vip/12.webp": "assets/assets/vip/12.e5cfef87.webp",
+  "assets/assets/vip/2.webp": "assets/assets/vip/2.091865a2.webp",
+  "assets/assets/vip/3.webp": "assets/assets/vip/3.a623f088.webp",
+  "assets/assets/vip/4.webp": "assets/assets/vip/4.f5bae50d.webp",
+  "assets/assets/vip/5.webp": "assets/assets/vip/5.ab482e2b.webp",
+  "assets/assets/vip/6.webp": "assets/assets/vip/6.35b39c6d.webp",
+  "assets/assets/vip/7.webp": "assets/assets/vip/7.55c325d1.webp",
+  "assets/assets/vip/8.webp": "assets/assets/vip/8.d24c2b80.webp",
+  "assets/assets/vip/9.webp": "assets/assets/vip/9.854365dc.webp",
+  "favicon.png": "favicon.5dcef449.png",
+  "flutter.js": "flutter.822458aa.js",
+  "main.dart.a3ccb398.b905917d.js": "main.dart.a3ccb398.b905917d.b905917d.js",
+  "main.dart.f11e1cc9.js": "main.dart.f11e1cc9.4e3284bf.js",
+  "main.dart.js": "main.dart.7eb29a33.js",
+  "main.dart_0.094ece36.js": "main.dart_0.094ece36.094ece36.js",
+  "main.dart_0.1b59f952.1b59f952.js": "main.dart_0.1b59f952.1b59f952.1b59f952.js",
+  "main.dart_0.js": "main.dart_0.787113bd.js",
+  "main.dart_1.251a3f7a.js": "main.dart_1.251a3f7a.251a3f7a.js",
+  "main.dart_1.f75715fe.f75715fe.js": "main.dart_1.f75715fe.f75715fe.f75715fe.js",
+  "main.dart_1.js": "main.dart_1.5c14e2d1.js",
+  "main.dart_2.1b1e90ab.1b1e90ab.js": "main.dart_2.1b1e90ab.1b1e90ab.1b1e90ab.js",
+  "main.dart_2.bae9f07e.js": "main.dart_2.bae9f07e.bae9f07e.js",
+  "main.dart_2.js": "main.dart_2.bf153669.js",
+  "main.dart_3.868224f7.868224f7.js": "main.dart_3.868224f7.868224f7.868224f7.js",
+  "main.dart_3.9c552fa3.js": "main.dart_3.9c552fa3.9c552fa3.js",
+  "main.dart_3.js": "main.dart_3.6c5dd5b4.js",
+  "main.dart_4.8d0aa7d0.8d0aa7d0.js": "main.dart_4.8d0aa7d0.8d0aa7d0.8d0aa7d0.js",
+  "main.dart_4.90e15d26.js": "main.dart_4.90e15d26.90e15d26.js",
+  "main.dart_4.js": "main.dart_4.8556c2be.js",
+  "main.dart_5.72d494bc.8084f0a9.js": "main.dart_5.72d494bc.8084f0a9.8084f0a9.js",
+  "main.dart_5.d1e12793.js": "main.dart_5.d1e12793.a4903ed9.js",
+  "main.dart_5.js": "main.dart_5.c6be6070.js",
+  "manifest.json": "manifest.0abe5cbd.json"
+};
+    
+    // hook dynamic create element `src` `href` property
+    (function(){
+      var _defineProperty = function (element, property) {
+        var value;
+        Object.defineProperty(element, property, {
+          get: function () {
+            return value;
+          },
+          set: function (v) {
+            value = v;
+            if (typeof v === 'string') {
+              if (v.startsWith('./')) {
+                v = v.replace('./', '');
+              }
+              if (hashFileManifest[v]) {
+                value = assetBase + hashFileManifest[v];
+              }
+            }
+            element.setAttribute(property, value);
+          }
+        })
+  
+        var _setAttribute = element.setAttribute;
+        element.setAttribute = function () {
+          var args = Array.prototype.slice.call(arguments);
+          // if (args[0] === 'src') {
+          //   //do something here
+          //   console.log('setAttribute', args[1])
+          // }
+          _setAttribute.apply(element, args);
+        }
+      }
+  
+      var _createElement = document.createElement;
+      var _hookProperties = {'link': 'href', 'script': 'src', 'audio': 'src', 'video': 'src', 'map': 'src', 'img': 'src'};
+      document.createElement = function (tagName) {
+        var element = _createElement.call(document, tagName);
+        var property = _hookProperties[tagName.toLowerCase()];
+        if (property) {
+          _defineProperty(element, property);
+        }
+        return element;
+      }  
+    })();
+
+    // load main.dart.js_xxx.part js
+    function dartDeferredLibraryLoader(uri, successCallback, errorCallback, loadId) {
+      console.info('uri: ' + uri + ', loadId: '+ loadId);
+      var src;
+      try {
+        var url = new URL(uri);
+        var key = url.pathname.replaceAll(/(.*)(main\.dart\.(.+)\.js)/g, '$2');
+        src = assetBase + mainjsManifest[key];
+      } catch (e) {
+        var key = uri.replaceAll(/(.*)(main\.dart\.(.+)\.js)/g, '$2');
+        src = assetBase + mainjsManifest[key];
+      }
+    
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = src;
+      script.addEventListener("load", successCallback, false);
+      script.addEventListener("error", errorCallback, false);
+      document.body.appendChild(script);
+    }
+    
